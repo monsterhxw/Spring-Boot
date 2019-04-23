b4884316673128487b11c2217ed1edf3c01cb633
monsterhxw <770342643@qq.com>
2019-04-23 09:00:07 +0800

    rebuild spring boot security oauth2 module

--- spring-boot-security-oauth/src/main/java/com/hxw/oauth/Application.java
+++ spring-boot-security-oauth/src/main/java/com/hxw/oauth/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityOauth2SampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityOauth2SampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
